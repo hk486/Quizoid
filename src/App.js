bff978e751950575790f5f7529c936531619a3f8
hk486 <meetpreet1207@gmail.com>
2021-12-30 02:04:01 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: 'url("/backk.jpg")' }}>
-      Hello
+    <BrowserRouter>
+    <div className="app" style={{ backgroundImage: 'url("/backk.jpg")' }}>
+      <Header/>
+
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+
+        </Route>
+      </Switch>
     </div>
+     <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
