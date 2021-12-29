00eb5254577fbf236ab2106a2ecffe8f64e917e9
hk486 <68745383+hk486@users.noreply.github.com>
2021-12-29 21:46:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundImage: 'url("/backk.jpg")' }}>
+      Hello
+    </div>
+  );
+}
+
+export default App;
