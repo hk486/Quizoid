27343bc2db8df361711b8e1601affa401598a2a3
hk486 <meetpreet1207@gmail.com>
2021-12-31 00:23:21 +0530

    fix

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,40 @@
+import { MenuItem, TextField } from '@material-ui/core';
 import React from 'react'
 import "./Home.css";
+import Categories, {} from "../../Data/Categories";
 const Home = () => {
-    return <div className='content'>
-        <div className='settings'>
-            <span style={{fontSize: 30}}>Quiz Settings</span>
+    return (
+        <div className="content">
+            <div className="settings">
+                <span style={{fontSize: 30}}>Quiz Settings</span>
+
+                <div className="settings_select">
+                    <TextField 
+                    style={{ marginBottom: 25}}
+                    label="Enter Your Name"
+                    variant="outlined"
+                    />
+
+                    <TextField 
+                    select label="Select Category" 
+                    variant="outlined" 
+                    style={{marginBottom:30}}>
+                    
+                    {
+                        Categories.map((cat)=>(
+                            <MenuItem key={cat.category} value={cat.value}>
+                                {cat.category}
+                            </MenuItem>
+                        ))
+                    }
+
+                    </TextField>
+
+                </div>
+            </div>
+            <img src="quiz.png" className="banner" alt="quiz img" />
         </div>
-        <img src='quiz.png' className='banner' alt='quiz img'/>
-    </div>
-    
+    );
 };
 
 export default Home;
